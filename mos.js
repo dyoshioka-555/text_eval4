53ed2e867b5127a435b222d911bd2889ccf721a7
dyoshioka-555 <yoshioka.daiki@g.sp.m.is.nagoya-u.ac.jp>
2021-07-02 13:23:31 +0900

    fix js and make.py

--- mos.js
+++ mos.js
@@ -42,20 +42,22 @@ function start_experiment() {
   Display()
 
   // read filepath
-  var origin_list = wav_dir + "set" + set_num + "/origin.list";
-  var method1_list = wav_dir + "set" + set_num + "/cvae.list";
-  var method2_list = wav_dir + "set" + set_num + "/cvae_bow.list";
-  var method3_list = wav_dir + "set" + set_num + "/cvae2.list";
-  var method4_list = wav_dir + "set" + set_num + "/cvae_bow2.list";
+  var origin_list = text_dir + "set" + set_num + "/origin.list";
+  var method1_list = text_dir + "set" + set_num + "/cvae.list";
+  var method2_list = text_dir + "set" + set_num + "/cvae_bow.list";
+  //var method3_list = text_dir + "set" + set_num + "/cvae2.list";
+  //var method4_list = text_dir + "set" + set_num + "/cvae_bow2.list";
   origin = loadText(origin_list);
   method1 = loadText(method1_list);
   method2 = loadText(method2_list);
-  method3 = loadText(method3_list);
-  method4 = loadText(method4_list);
+  //method3 = loadText(method3_list);
+  //method4 = loadText(method4_list);
   outfile = name + "_set" + set_num + ".csv";
-  file_list = makeFileList()
-  console.log(file_list);
-  scores = (new Array(file_list.length)).fill(0);
+  text_list = makeTextList()
+  console.log(text_list);
+  scores1 = (new Array(text_list.length)).fill(0);
+  scores2 = (new Array(text_list.length)).fill(0);
+  scores3 = (new Array(text_list.length)).fill(0);
   eval1 = document.getElementsByName("eval1");
   eval2 = document.getElementsByName("eval2");
   eval3 = document.getElementsByName("eval3");
@@ -81,54 +83,86 @@ function loadText(filename) {
 }
 
 // make file list
-function makeFileList() {
-  var files = origin;
-  files = files.concat(method1, method2, method3, method4)
-  files.shuffle();
-  return files;
+function makeTextList() {
+  var texts = origin;
+  texts = texts.concat(method1, method2, method3, method4)
+  texts.shuffle();
+  return texts;
 }
 
-function setAudio() {
-  document.getElementById("page").textContent = "" + (n + 1) + "/" + scores.length;
+function setText() {
+  document.getElementById("page").textContent = "" + (n + 1) + "/" + scores1.length;
 
-  document.getElementById("audio").innerHTML = 'Voice:<br>'
-    + '<audio src="' + file_list[n]
-    + '" controls preload="auto">'
-    + '</audio>';
+  document.getElementById("text").innerHTML = text_list[n];
 }
 
 function init() {
   n = 0;
-  setAudio();
-  evalCheck();
+  SetText();
+  evalCheck1();
+  evalCheck2();
+  evalCheck3();
   setButton();
 }
 
-function evalCheck() {
-  const c = scores[n];
-  if ((c <= 0) || (c > eval.length)) {
-    for (var i = 0; i < eval.length; i++) {
-      eval[i].checked = false;
+function evalCheck1() {
+  const c = scores1[n];
+  if ((c <= 0) || (c > eval1.length)) {
+    for (var i = 0; i < eval1.length; i++) {
+      eval1[i].checked = false;
     }
   }
   else {
-    eval[c - 1].checked = true;
+    eval1[c - 1].checked = true;
+  }
+}
+function evalCheck2() {
+  const c = scores2[n];
+  if ((c <= 0) || (c > eval2.length)) {
+    for (var i = 0; i < eval2.length; i++) {
+      eval2[i].checked = false;
+    }
+  }
+  else {
+    eval2[c - 1].checked = true;
+  }
+}
+function evalCheck3() {
+  const c = scores3[n];
+  if ((c <= 0) || (c > eval3.length)) {
+    for (var i = 0; i < eval3.length; i++) {
+      eval3[i].checked = false;
+    }
+  }
+  else {
+    eval3[c - 1].checked = true;
   }
 }
 
 function setButton() {
-  if (n == (scores.length - 1)) {
+  var finish_flag = 0;
+  if (n == (scores1.length - 1)) {
     document.getElementById("prev").disabled = false;
     document.getElementById("next2").disabled = true;
     document.getElementById("finish").disabled = true;
     for (var i = 0; i < eval.length; i++) {
-      if (eval[i].checked) {
+      if (eval1[i].checked) {
+        finish_flag += 1;
+      }
+      if (eval2[i].checked) {
+        finish_flag += 1;
+      }
+      if (eval3[i].checked) {
+        finish_flag += 1;
+      }
+      if (finish_flag == 3) {
         document.getElementById("finish").disabled = false;
         break;
       }
     }
   }
   else {
+    var next_flag = 0;
     if (n == 0) {
       document.getElementById("prev").disabled = true;
     }
@@ -138,7 +172,16 @@ function setButton() {
     document.getElementById("next2").disabled = true;
     document.getElementById("finish").disabled = true;
     for (var i = 0; i < eval.length; i++) {
-      if (eval[i].checked) {
+      if (eval1[i].checked) {
+        next_flag += 1;
+      }
+      if (eval2[i].checked) {
+        next_flag += 1;
+      }
+      if (eval3[i].checked) {
+        next_flag += 1;
+      }
+      if (next_flag == 3) {
         document.getElementById("next2").disabled = false;
         break;
       }
@@ -146,10 +189,24 @@ function setButton() {
   }
 }
 
-function evaluation() {
-  for (var i = 0; i < eval.length; i++) {
-    if (eval[i].checked) {
-      scores[n] = 5 - i;
+function evaluation(k) {
+  for (var i = 0; i < eval1.length; i++) {
+    switch (k) {
+      case 1:
+        if (eval1[i].checked) {
+          scores1[n] = i;
+        }
+        break;
+      case 2:
+        if (eval2[i].checked) {
+          scores2[n] = i;
+        }
+        break;
+      case 3:
+        if (eval1[i].checked) {
+          scores3[n] = i;
+        }
+        break;
     }
   }
   setButton();
@@ -157,9 +214,11 @@ function evaluation() {
 
 function exportCSV() {
   var csvData = "";
-  for (var i = 0; i < file_list.length; i++) {
-    csvData += "" + file_list[i] + ","
-      + scores[i] + "\r\n";
+  for (var i = 0; i < text_list.length; i++) {
+    csvData += "" + text_list[i] + ","
+      + scores1[i] + ","
+      + scores2[i] + ","
+      + scores3[i] + "\r\n";
   }
 
   const link = document.createElement("a");
@@ -176,15 +235,19 @@ function exportCSV() {
 
 function next() {
   n++;
-  setAudio();
-  evalCheck();
+  SetText();
+  evalCheck1();
+  evalCheck2();
+  evalCheck3();
   setButton();
 }
 
 function prev() {
   n--;
-  setAudio();
-  evalCheck();
+  SetText();
+  evalCheck1();
+  evalCheck2();
+  evalCheck3();
   setButton();
 }
 
@@ -196,7 +259,7 @@ function finish() {
 // --------- 設定 --------- //
 
 // directory name
-const wav_dir = "wav/";
+const text_dir = "texts/";
 
 // invalid enter key
 document.onkeypress = invalid_enter();
@@ -204,11 +267,13 @@ document.onkeypress = invalid_enter();
 var origin;
 var method1;
 var method2;
-var method3;
-var method4;
+//var method3;
+//var method4;
 var outfile;
-var file_list;
-var scores;
+var text_list;
+var scores1;
+var scores2;
+var scores3;
 
 // ローカルで行う場合はloadText()は動作しないため
 var n = 0;
